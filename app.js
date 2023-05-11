6a65a1e6b94b08ccd981c7d5dacfb95212655663
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-05-11 20:29:58 +0700

    fix bugs preventdefault

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 function showAlert() {
     alert("Take action alert box!");
-}
-
-
-function sendData(){
-
+  }
+  
+  function sendData() {
     try {
-        var name =  document.getElementById("name").value;
+      document.getElementById("formsaran").addEventListener("submit", (e) => {
+        e.preventDefault();
+        var name = document.getElementById("name").value;
         var email = document.getElementById("email").value;
         var message = document.getElementById("message").value;
-
+  
         document.getElementById("nameShow").innerText = name;
         document.getElementById("emailShow").innerText = email;
         document.getElementById("messageShow").innerText = message;
-        
-      }
-      catch(err) {
-        alert(err.message);
-      }
-
-
-    
-}
+      });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
