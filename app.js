0f0fd2c24bded3a09f747f70b1afdc49e0cf32cc
Rasyid Kusnady <rasyidkusnady@gmail.com>
2023-05-25 15:24:00 +0700

    add calculator

--- app.js
+++ app.js
@@ -26,3 +26,55 @@ function showAlert() {
       alert(err.message);
     }
   }
+
+
+  var items = [
+    { name: "Botol Plastik", quantity: 0 },
+    { name: "Sedotan", quantity: 0 },
+    { name: "Puntung Rokok", quantity: 0 }
+  ];
+
+  function updateQuantity(index, quantity) {
+    items[index].quantity = parseInt(quantity);
+  }
+
+  function addItem() {
+    var itemName = prompt("Enter item name");
+    if (itemName) {
+      var newItem = {
+        name: itemName,
+        quantity: 0
+      };
+      items.push(newItem);
+      updateItemList();
+    }
+  }
+
+  function updateItemList() {
+    var itemList = document.getElementById("itemList");
+    itemList.innerHTML = "";
+
+    for (var i = 0; i < items.length; i++) {
+      var listItem = document.createElement("li");
+      listItem.textContent = items[i].name + ": ";
+      var quantityInput = document.createElement("input");
+      quantityInput.type = "number";
+      quantityInput.min = 0;
+      quantityInput.value = items[i].quantity;
+      quantityInput.onchange = (function(index) {
+        return function() {
+          updateQuantity(index, this.value);
+        };
+      })(i);
+      listItem.appendChild(quantityInput);
+      itemList.appendChild(listItem);
+    }
+  }
+
+  function calculateTotal() {
+    var total = 0;
+    for (var i = 0; i < items.length; i++) {
+      total += items[i].quantity;
+    }
+    document.getElementById("total").textContent = "Total Sampah: " + total;
+  }
